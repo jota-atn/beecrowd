9babe39898b0ebc550ef34c532ad6d702aca84c6
João Antonio <jamqueiroz2005@gmail.com>
2024-11-22 00:53:17 -0300

    Update 2006.java

--- iniciante/java/2006/2006.java
+++ iniciante/java/2006/2006.java
@@ -1,27 +1,27 @@
 mport java.util.Scanner;
 
 public class Main {
-	    public static void main(String[] args) {
+	public static void main(String[] args) {
 		            
-		            Scanner sc = new Scanner(System.in);
+		Scanner sc = new Scanner(System.in);
 			            
-			            int t = sc.nextInt();
+		int t = sc.nextInt();
 				            
-				            int[] inputs = new int[5];
+		int[] inputs = new int[5];
 					            
-					            for (int i = 0; i < inputs.length; i++) {
-							                inputs[i] = sc.nextInt();
-									        }
+		for (int i = 0; i < inputs.length; i++) {
+			inputs[i] = sc.nextInt();
+		}
 						            
-						            int counter = 0;
+		int counter = 0;
 							            
-							            for (int i = 0; i < inputs.length; i++) {
-									                if (inputs[i] == t) {
-												                counter++;
-														            }
-											        }
+		for (int i = 0; i < inputs.length; i++) {
+			if (inputs[i] == t) {
+				counter++;
+			}
+		}
 								            
-								            System.out.println(counter);
+		System.out.println(counter);
 									            
-									        }
+	}
 }
